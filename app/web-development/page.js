c423d1ddea6b4a13e395fa9149e822905420ee8a
Yuvraj Singh thapa <iamkunal_st@Yuvrajs-MacBook-Pro.local>
2025-05-07 11:17:06 +0530

    evolkunedit

--- app/web-development/page.js
+++ app/web-development/page.js
@@ -1,5 +1,6 @@
-import WordPressForm from '@/components/Services/web-development/WordPressForm';
+//import WordPressForm from '@/components/Services/web-development/WordPressForm';
+import WebDevMainForm from '@/components/Services/web-development/WebDevMainForm';
 
 export default function WebDevRoute() {
-  return <WordPressForm />;
+  return <WebDevMainForm/>;
 }
